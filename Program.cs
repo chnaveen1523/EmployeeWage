36c5f89fd6313968c19d7f48a4e874c4bb2c2c68
bannu-code-star <nitinchiliveri007@gmail.com>
2021-09-03 08:28:15 +0530

    Added EmpWage

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // UC1 EmployeeWage
+            int IS_PRESENT = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == IS_PRESENT)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+            Console.ReadLine();
+        }
+    }
+}
